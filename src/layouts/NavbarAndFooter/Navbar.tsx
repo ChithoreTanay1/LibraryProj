42edfd1a2c6fb82acfbeb0296394271bb398b2e9
Chithore Tanay <tanaychithore@mailbox.unideb.hu>
2025-04-26 00:57:27 +0200

    Fixed Stuff

--- src/layouts/NavbarAndFooter/Navbar.tsx
+++ src/layouts/NavbarAndFooter/Navbar.tsx
@@ -7,7 +7,7 @@ export const Navbar = () => {
                     className='navbar-toggler' 
                     type='button'
                     data-bs-toggle='collapse' 
-                    data-bs-target='#navbarNavDropdown'  // Fixed hyphenation
+                    data-bs-target='#navbarNavDropdown' 
                     aria-controls='navbarNavDropdown' 
                     aria-expanded='false'
                     aria-label='Toggle navigation'
@@ -15,7 +15,7 @@ export const Navbar = () => {
                     <span className='navbar-toggler-icon'></span>
                 </button>
                 
-                <div className='collapse navbar-collapse' id='navbarNavDropdown'>  // Fixed ID match
+                <div className='collapse navbar-collapse' id='navbarNavDropdown'> 
                     <ul className='navbar-nav'>
                         <li className='nav-item'>
                             <a className='nav-link' href='#'>Home</a>
